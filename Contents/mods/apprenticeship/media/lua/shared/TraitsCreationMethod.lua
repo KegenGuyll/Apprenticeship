51eb95ca20573f28a7cc1f87e65d30f6b85b8298
Kegen Guyll <guyllkegen@gmail.com>
2024-07-10 14:57:08 -0500

    created init traits

---
+++ Contents/mods/apprenticeship/media/lua/shared/TraitsCreationMethod.lua
@@ -0,0 +1,12 @@
+require('NPCs/MainCreationMethods');
+
+local function  initTraits()
+  local savant = TraitFactory.addTrait("savant", getText("UI_trait_savant"), 1, getText("UI_trait_savantdesc"), false, false);
+  local professor = TraitFactory.addTrait("professor", getText("UI_trait_professor"), 3, getText("UI_trait_professordesc"), false, false);
+
+  local badTeacher = TraitFactory.addTrait("badTeacher", getText("UI_trait_badTeacher"), -1, getText("UI_trait_badTeacherdesc"), false, false);
+  local classDismissed = TraitFactory.addTrait("classDismissed", getText("UI_trait_classDismissed"), -3, getText("UI_trait_classDismisseddesc"), false, false);
+end
+
+
+Events.OnGameBoot.Add(initTraits);
